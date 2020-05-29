5e502f0826c1586e83822fb5b5ceca547b07a699
nikogrobles <66100028+nikogrobles@users.noreply.github.com>
2020-05-28 23:27:16 -0700

    extract main running function to roo.js

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
-import Discord from 'discord.js';
 import dotenv from 'dotenv';
+import { run as runRooBot } from './src/roo';
 
 dotenv.config();
 
-const client = new Discord.Client()
-
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-
-const TEXT_MATCHERS = {
-    FuckNiko: new RegExp("fuck niko", "gi"),
-};
-
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!")
-
-  } else if (TEXT_MATCHERS.FuckNiko.test(msg.content)) {
-    msg.reply("Fuck you :)")
-  }
-})
-
-client.login(process.env.DISCORD_BOT_TOKEN);
+runRooBot();
--- src/roo.js
+++ src/roo.js
@@ -1,14 +1,29 @@
 import Discord from 'discord.js';
 import dotenv from 'dotenv';
+import * as routers from './src/router';
+import MessagesRouter from './routers/messages';
 
 dotenv.config();
+
+const EVENT_ROUTER_MAP = {
+    message: MessagesRouter
+};
 
 export default class RooBot {
     discordClient = new Discord.Client();
 
+    #routerRegistry = new Map();
+
     constructor(options) {
         this.discordClient = new Discord.Client(options);
         this.#attachEvents();
+    }
+
+    registerRouters(routers) {
+        Object.keys(routers).forEach(routerName => {
+            const router = routers[routerName];
+            this.#registerRouter(router);
+        });
     }
 
     onReady() {
@@ -19,12 +34,30 @@ export default class RooBot {
         return this.discordClient.login(token);
     }
 
-    #attachEvents() {
-        this.discordClient.on('ready', () => this.onReady());
-        this.discordClient.on('message', msg => this.messagesRouter.routeMessage(msg));
+    #registerRouter(router) {
+        const routerType = typeof router;
+        const routers = this.#routerRegistry.get(routerType) || [];
+
+        if (!routers.includes(router)) {
+            this.#routerRegistry.set(routerType, [...routers, router])
+        }
     }
 
-    #routeMessage() {
-        this.messagesRouter
+    #attachEvents() {
+        this.discordClient.on('ready', () => this.onReady());
+
+        Object.keys(EVENT_ROUTER_MAP).forEach(eventName => {
+            this.discordClient.on(eventName, (...args) => {
+                const routerType = EVENT_ROUTER_MAP[eventName];
+                const routers = this.#routerRegistry.get(routerType) || [];
+                routers.forEach(router => router.dispatch(...args));
+            });
+        });
     }
+}
+
+export function run() {
+    const rooBot = new RooBot();
+    rooBot.registerRouters(routers);
+    rooBot.login(process.env.DISCORD_BOT_TOKEN);      
 }
