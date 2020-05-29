a957745dccfd8deeb5ad0179a08b37860bec60c5
nikogrobles <66100028+nikogrobles@users.noreply.github.com>
2020-05-28 19:49:53 -0700

    Update index.js
    extract token to env var

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import Discord from 'discord.js';
- 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const client = new Discord.Client()
 
 client.on("ready", () => {
@@ -19,4 +22,4 @@ client.on("message", msg => {
   }
 })
 
-client.login("NzE1NzI3MTgyNzkzODAxNzI5.XtBbMg.ATXy16YnidlqkM5CrIkjJPCBz0s")
+client.login(process.env.DISCORD_BOT_TOKEN);
