c55d2c92fe10054365d8e6e9d43c29019bfcebea
albertyang <albertesyang@gmail.com>
2020-06-19 23:54:03 -0400

    added wavy command

--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import MessagesRouter from './routers/messages.js';
 import HugCommand from './commands/hug.js';
 import PartyCommand from './commands/party.js';
 import PurgeCommand from './commands/purge.js';
+import WavyCommand from './commands/wavy.js';
 
 const messagesRouter = MessagesRouter.createRoutes(function() {
 
     this.matchCommand('hug', HugCommand);
     this.matchCommand('party', PartyCommand);
     this.matchCommand('purge', PurgeCommand);
+    this.matchCommand('wavy', WavyCommand);
 
     this.matchExtact('ping', async (message) => {
         message.reply("Pong!");
